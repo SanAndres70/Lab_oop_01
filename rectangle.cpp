136f39ac3445f3db53b7cef0a19bafe1c63bab87
SanAndres70 <Andrew70popov@mail.ru>
2025-04-21 19:34:54 +0300

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -2,17 +2,21 @@
 #include <cmath>
 
 Rectangle::Rectangle(const string& name, const Point& p1, const Point& p2)
-    : Figure(name), p1(p1), p2(p2) {}
+    : Figure(name), p1(p1), p2(p2) {
+    if (p2.get_x() <= p1.get_x() || p2.get_y() <= p1.get_y()) {
+        throw invalid_argument("Некорректные координаты: p2 должна быть правее и выше p1");
+    }
+}
 
 string Rectangle::getName() const {
     return name;
 }
 
 string Rectangle::getDescription() const {
-    return "Прямоугольник с углами (" + to_string(p1.x) + ", " + to_string(p1.y) +
-           ") и (" + to_string(p2.x) + ", " + to_string(p2.y) + ")";
+    return "Прямоугольник с углами (" + to_string(p1.get_x()) + ", " + to_string(p1.get_y()) +
+           ") и (" + to_string(p2.get_x()) + ", " + to_string(p2.get_y()) + ")";
 }
 
 double Rectangle::getArea() const {
-    return abs((p2.x - p1.x) * (p2.y - p1.y));
+    return (p2.get_x() - p1.get_x()) * (p2.get_y() - p1.get_y());
 }
