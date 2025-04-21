9a0d690149dea08a0d0f34f3ac45d671cb2bcfe5
SanAndres70 <Andrew70popov@mail.ru>
2025-04-21 19:32:49 +0300

    Update point.h

--- point.h
+++ point.h
@@ -4,6 +4,9 @@
 class Point {
 public:
     Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+    double get_x() const { return x; }
+    double get_y() const { return y; }
+
 private:
     double x, y;
 };
